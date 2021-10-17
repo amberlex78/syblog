1cda26d7141234cda3a501fdd190a91068660c57
amberlex78 <amberlex78@gmail.com>
2021-10-17 18:16:35 +0300

    add getIsVerified for User

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -208,4 +208,9 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
 
         return $this;
     }
+
+    public function getIsVerified(): bool
+    {
+        return $this->isVerified;
+    }
 }
