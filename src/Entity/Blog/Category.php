c08988a44323d99e14ebbfffb88ecfa6b6038af1
amberlex78 <amberlex78@gmail.com>
2021-10-19 01:00:25 +0300

    add Blog entities (Category, Post)

---
+++ src/Entity/Blog/Category.php
@@ -0,0 +1,220 @@
+<?php
+
+namespace App\Entity\Blog;
+
+use App\Repository\Blog\CategoryRepository;
+use DateTimeImmutable;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\ORM\Mapping as ORM;
+use JetBrains\PhpStorm\Pure;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
+use Symfony\Component\Validator\Constraints as Assert;
+
+#[ORM\Entity(repositoryClass: CategoryRepository::class)]
+#[ORM\HasLifecycleCallbacks]
+#[ORM\Table('blog_categories')]
+#[UniqueEntity(fields: ['slug'], message: 'There is already an page with this slug')]
+class Category
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column(type: 'integer')]
+    private int $id;
+
+    #[ORM\OneToMany(mappedBy: 'category', targetEntity: Post::class)]
+    private Collection $posts;
+
+    #[ORM\Column(type: 'string', length: 255)]
+    #[Assert\NotBlank(message: 'Please enter a name.')]
+    private string $name;
+
+    #[ORM\Column(type: 'string', length: 255)]
+    #[Assert\NotBlank(message: 'Please enter a slug.')]
+    private string $slug;
+
+    #[ORM\Column(type: 'text', nullable: true)]
+    private ?string $description;
+
+    #[ORM\Column(type: 'string', length: 255, nullable: true)]
+    private ?string $image;
+
+    #[ORM\Column(type: 'string', length: 255, nullable: true)]
+    private ?string $seo_title;
+
+    #[ORM\Column(type: 'string', length: 255, nullable: true)]
+    private ?string $seo_keywords;
+
+    #[ORM\Column(type: 'string', length: 255, nullable: true)]
+    private ?string $seo_description;
+
+    #[ORM\Column(type: 'datetime_immutable')]
+    private DateTimeImmutable $created_at;
+
+    #[ORM\Column(type: 'datetime_immutable')]
+    private DateTimeImmutable $updated_at;
+
+    #[Pure]
+    public function __construct()
+    {
+        $this->posts = new ArrayCollection();
+    }
+
+    public function __toString(): string
+    {
+        return $this->name;
+    }
+
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getSlug(): string
+    {
+        return $this->slug;
+    }
+
+    public function setSlug(string $slug): self
+    {
+        $this->slug = $slug;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(?string $description): self
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    public function getImage(): ?string
+    {
+        return $this->image;
+    }
+
+    public function setImage(?string $image): self
+    {
+        $this->image = $image;
+
+        return $this;
+    }
+
+    public function getSeoTitle(): ?string
+    {
+        return $this->seo_title;
+    }
+
+    public function setSeoTitle(?string $seo_title): self
+    {
+        $this->seo_title = $seo_title;
+
+        return $this;
+    }
+
+    public function getSeoKeywords(): ?string
+    {
+        return $this->seo_keywords;
+    }
+
+    public function setSeoKeywords(?string $seo_keywords): self
+    {
+        $this->seo_keywords = $seo_keywords;
+
+        return $this;
+    }
+
+    public function getSeoDescription(): ?string
+    {
+        return $this->seo_description;
+    }
+
+    public function setSeoDescription(?string $seo_description): self
+    {
+        $this->seo_description = $seo_description;
+
+        return $this;
+    }
+
+    public function getCreatedAt(): DateTimeImmutable
+    {
+        return $this->created_at;
+    }
+
+    public function setCreatedAt(DateTimeImmutable $created_at): self
+    {
+        $this->created_at = $created_at;
+
+        return $this;
+    }
+
+    public function getUpdatedAt(): DateTimeImmutable
+    {
+        return $this->updated_at;
+    }
+
+    public function setUpdatedAt(DateTimeImmutable $updated_at): self
+    {
+        $this->updated_at = $updated_at;
+
+        return $this;
+    }
+
+    #[ORM\PrePersist]
+    public function prePersist()
+    {
+        $this->setCreatedAt(new DateTimeImmutable());
+        $this->setUpdatedAt(new DateTimeImmutable());
+    }
+
+    #[ORM\PreUpdate]
+    public function preUpdate()
+    {
+        $this->setUpdatedAt(new DateTimeImmutable());
+    }
+
+    public function getPosts(): Collection
+    {
+        return $this->posts;
+    }
+
+    public function addPost(Post $post): self
+    {
+        if (!$this->posts->contains($post)) {
+            $this->posts[] = $post;
+            $post->setCategory($this);
+        }
+
+        return $this;
+    }
+
+    public function removePost(Post $post): self
+    {
+        if ($this->posts->removeElement($post)) {
+            // set the owning side to null (unless already changed)
+            if ($post->getCategory() === $this) {
+                $post->setCategory(null);
+            }
+        }
+
+        return $this;
+    }
+}
---
+++ src/Entity/Blog/Post.php
@@ -0,0 +1,221 @@
+<?php
+
+namespace App\Entity\Blog;
+
+use App\Repository\Blog\PostRepository;
+use DateTimeImmutable;
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
+use Symfony\Component\Validator\Constraints as Assert;
+
+#[ORM\Entity(repositoryClass: PostRepository::class)]
+#[ORM\HasLifecycleCallbacks]
+#[ORM\Table('blog_posts')]
+#[UniqueEntity(fields: ['slug'], message: 'There is already an page with this slug')]
+class Post
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column(type: 'integer')]
+    private int $id;
+
+    #[ORM\ManyToOne(targetEntity: Category::class, inversedBy: 'posts')]
+    private Category $category;
+
+    #[ORM\Column(type: 'string', length: 255)]
+    #[Assert\NotBlank(message: 'Please enter a title.')]
+    private string $title;
+
+    #[ORM\Column(type: 'string', length: 255)]
+    #[Assert\NotBlank(message: 'Please enter a slug.')]
+    private string $slug;
+
+    #[ORM\Column(type: 'text', nullable: true)]
+    private ?string $preview;
+
+    #[ORM\Column(type: 'text', nullable: true)]
+    private ?string $content;
+
+    #[ORM\Column(type: 'string', length: 255, nullable: true)]
+    private ?string $image;
+
+    #[ORM\Column(type: 'boolean')]
+    private bool $is_draft;
+
+    #[ORM\Column(type: 'string', length: 255, nullable: true)]
+    private ?string $seo_title;
+
+    #[ORM\Column(type: 'string', length: 255, nullable: true)]
+    private ?string $seo_keywords;
+
+    #[ORM\Column(type: 'string', length: 255, nullable: true)]
+    private ?string $seo_description;
+
+    #[ORM\Column(type: 'datetime_immutable')]
+    private DateTimeImmutable $created_at;
+
+    #[ORM\Column(type: 'datetime_immutable')]
+    private DateTimeImmutable $updated_at;
+
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    public function getTitle(): string
+    {
+        return $this->title;
+    }
+
+    public function setTitle(string $title): self
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    public function getSlug(): string
+    {
+        return $this->slug;
+    }
+
+    public function setSlug(string $slug): self
+    {
+        $this->slug = $slug;
+
+        return $this;
+    }
+
+    public function getPreview(): ?string
+    {
+        return $this->preview;
+    }
+
+    public function setPreview(?string $preview): self
+    {
+        $this->preview = $preview;
+
+        return $this;
+    }
+
+    public function getContent(): ?string
+    {
+        return $this->content;
+    }
+
+    public function setContent(?string $content): self
+    {
+        $this->content = $content;
+
+        return $this;
+    }
+
+    public function getImage(): ?string
+    {
+        return $this->image;
+    }
+
+    public function setImage(?string $image): self
+    {
+        $this->image = $image;
+
+        return $this;
+    }
+
+    public function getIsDraft(): bool
+    {
+        return $this->is_draft;
+    }
+
+    public function setIsDraft(bool $is_draft): self
+    {
+        $this->is_draft = $is_draft;
+
+        return $this;
+    }
+
+    public function getSeoTitle(): ?string
+    {
+        return $this->seo_title;
+    }
+
+    public function setSeoTitle(?string $seo_title): self
+    {
+        $this->seo_title = $seo_title;
+
+        return $this;
+    }
+
+    public function getSeoKeywords(): ?string
+    {
+        return $this->seo_keywords;
+    }
+
+    public function setSeoKeywords(?string $seo_keywords): self
+    {
+        $this->seo_keywords = $seo_keywords;
+
+        return $this;
+    }
+
+    public function getSeoDescription(): ?string
+    {
+        return $this->seo_description;
+    }
+
+    public function setSeoDescription(?string $seo_description): self
+    {
+        $this->seo_description = $seo_description;
+
+        return $this;
+    }
+
+    public function getCreatedAt(): DateTimeImmutable
+    {
+        return $this->created_at;
+    }
+
+    public function setCreatedAt(DateTimeImmutable $created_at): self
+    {
+        $this->created_at = $created_at;
+
+        return $this;
+    }
+
+    public function getUpdatedAt(): DateTimeImmutable
+    {
+        return $this->updated_at;
+    }
+
+    public function setUpdatedAt(DateTimeImmutable $updated_at): self
+    {
+        $this->updated_at = $updated_at;
+
+        return $this;
+    }
+
+    #[ORM\PrePersist]
+    public function prePersist()
+    {
+        $this->setCreatedAt(new DateTimeImmutable());
+        $this->setUpdatedAt(new DateTimeImmutable());
+    }
+
+    #[ORM\PreUpdate]
+    public function preUpdate()
+    {
+        $this->setUpdatedAt(new DateTimeImmutable());
+    }
+
+    public function getCategory(): ?Category
+    {
+        return $this->category;
+    }
+
+    public function setCategory(?Category $category): self
+    {
+        $this->category = $category;
+
+        return $this;
+    }
+}
