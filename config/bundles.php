1a68e293487ce88e2e2fee65664ba5865e7407fe
amberlex78 <amberlex78@gmail.com>
2021-10-20 21:56:15 +0300

    composer req liip/imagine-bundle, thumbnail_tiny for Post list

--- config/bundles.php
+++ config/bundles.php
@@ -18,4 +18,5 @@ return [
     FOS\CKEditorBundle\FOSCKEditorBundle::class => ['all' => true],
     Knp\Bundle\PaginatorBundle\KnpPaginatorBundle::class => ['all' => true],
     FM\ElfinderBundle\FMElfinderBundle::class => ['all' => true],
+    Liip\ImagineBundle\LiipImagineBundle::class => ['all' => true],
 ];
