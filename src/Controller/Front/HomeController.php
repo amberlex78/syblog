592d0112e3f2f14704abdd55725d5f5c49f64564
amberlex78 <amberlex78@gmail.com>
2021-10-29 17:53:30 +0300

    start template for blog

--- src/Controller/Front/HomeController.php
+++ src/Controller/Front/HomeController.php
@@ -2,17 +2,25 @@
 
 namespace App\Controller\Front;
 
+use App\Repository\Blog\PostRepository;
+use Knp\Component\Pager\PaginatorInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class HomeController extends AbstractController
 {
     #[Route('', name: 'front_home', methods: ['GET'])]
-    public function index(): Response
+    public function index(Request $request, PostRepository $postRepository, PaginatorInterface $paginator): Response
     {
+        $posts = $paginator->paginate(
+            $postRepository->findBy(['isDraft' => false], ['id' => 'desc']),
+            $request->query->getInt('page', 1)
+        );
+
         return $this->render('front/home/index.html.twig', [
-            'controller_name' => 'HomeController',
+            'posts' => $posts,
         ]);
     }
 }
