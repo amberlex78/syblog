addc87e447e5267fc8562cbb2675f8bde3f0e494
amberlex78 <amberlex78@gmail.com>
2021-11-06 13:31:47 +0200

    add the ability change password in profile (front)

--- src/Form/Front/ProfileEditFormType.php
+++ src/Form/Front/ProfileEditFormType.php
@@ -4,17 +4,36 @@ namespace App\Form\Front;
 
 use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\Length;
 
 class ProfileEditFormType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            //->add('password')
             ->add('firstName')
             ->add('lastName')
+            ->add('plainPassword', RepeatedType::class, [
+                'mapped' => false,
+                'required' => false,
+                'type' => PasswordType::class,
+                'invalid_message' => 'The passwords do not match.',
+                'first_options' => [
+                    'label' => 'New password',
+                    'attr' => ['placeholder' => 'Password'],
+                    'constraints' => [
+                        new Length(['min' => 6, 'max' => 128]),
+                    ],
+                ],
+                'second_options' => [
+                    'label' => 'Confirm password',
+                    'attr' => ['placeholder' => 'Confirm Password']
+                ],
+            ])
         ;
     }
 
